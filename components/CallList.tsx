a4ecd85da0c83b29ca61e2b093f9f4b9f8dd952a
Aryansh Malviya <aryanshmalviya2626@gmail.com>
2024-04-30 18:47:27 +0530

    join meeting modal and project completion

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -71,7 +71,7 @@ const CallList = ({type} : {type : 'ended' | 'upcoming' | 'recordings'}) => {
             type === 'upcoming' ? '/icons/upcoming.svg' :
             'icons/recordings.svg'
           }
-          title={(meeting as Call).state?.custom.description.substring(0,25) || meeting.filename.substring(0,20) ||'No Description'}
+          title={(meeting as Call).state?.custom?.description?.substring(0,25) || meeting?.filename?.substring(0,20) ||'No Description'}
           date={meeting.state?.startsAt.toLocaleString() || meeting.start_time.toLocaleString()}
           isPreviousMeeting={type === 'ended'}
           buttonIcon1={type === 'recordings' ? '/icons/play.svg': undefined }
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -10,6 +10,7 @@ import { useToast } from './ui/use-toast'
 import { Textarea } from './ui/textarea'
 
 import ReactDatePicker from 'react-datepicker'
+import { Input } from './ui/input'
 
 const MeetingTypeList = () => {
     const router = useRouter();
@@ -87,7 +88,7 @@ const MeetingTypeList = () => {
             img='/icons/recordings.svg'
             title='View Recordings'
             description='Check out your recordings'
-            handleClick= {()=> setMeetingState('isJoiningMeeting')}
+            handleClick= {()=>  router.push('/recordings')}
             className="bg-purple-1"
             
        />
@@ -156,6 +157,21 @@ const MeetingTypeList = () => {
           buttonText="Start meeting"
           handleClick={createMeeting}
        />
+
+          <MeetingModal
+               isOpen={meetingState === "isJoiningMeeting"}
+               onClose={()=> setMeetingState(undefined)}
+               title="Type the link here"
+               className="text-center"
+               buttonText="Join Meeting"
+               handleClick={()=> router.push(values.link)}
+          >
+               <Input
+                    placeholder='Meeting Link'
+                    className='border-none bg-dark-3 focus-visible:ring-0 focus-visible: ring-offset-0'
+                    onChange={(e) => setValues({...values, link: e.target.value})}
+               />
+          </MeetingModal>
     </section>
   )
 }
